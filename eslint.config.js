60b602d37270eaa98f0522fe94a01ee4374b68c2
asafg <asafg@ai21.com>
2024-11-07 10:36:58 +0200

    fix: Precommit

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,18 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
-  {rules: {
-    "semi": "off",
-    "no-unreachable": "error", // Already in ts, but perhaps it catches more cases
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/strict-boolean-expressions": "off", // Often works incorrectly
-      "@typescript-eslint/no-floating-promises": "off",
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.browser } },
+  {
+    rules: {
+      semi: 'off',
+      'no-unreachable': 'error', // Already in ts, but perhaps it catches more cases
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/strict-boolean-expressions': 'off', // Often works incorrectly
+      '@typescript-eslint/no-floating-promises': 'off',
     },
   },
   pluginJs.configs.recommended,
