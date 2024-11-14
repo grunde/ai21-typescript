d62200222260ad3a0055f88082dce17596cc644e
asafg <asafg@ai21.com>
2024-11-14 09:57:17 +0200

    refactor: Removed comments

--- rollup.config.js
+++ rollup.config.js
@@ -4,24 +4,24 @@ import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
 
 export default {
-  input: 'src/index.ts', // Entry point of your library
+  input: 'src/index.ts',
   output: [
     {
       file: 'dist/bundle.cjs.js',
-      format: 'cjs', // CommonJS format for Node.js
+      format: 'cjs',
       sourcemap: true,
     },
     {
       file: 'dist/bundle.esm.js',
-      format: 'esm', // ES module format for modern browsers
+      format: 'esm',
       sourcemap: true,
     },
   ],
   plugins: [
-    resolve(), // Resolves node_modules
-    commonjs(), // Converts CommonJS to ES6
-    typescript(), // Compiles TypeScript
-    terser(), // Minifies the bundle
+    resolve(),
+    commonjs(),
+    typescript(),
+    terser(),
   ],
-  external: ['node-fetch'], // Exclude node-fetch from the bundle
+  external: ['node-fetch'],
 };
