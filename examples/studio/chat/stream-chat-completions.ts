e9b3cec3c1695df7eb0beb12df84ab061d97518f
asafg <asafg@ai21.com>
2024-11-18 11:47:56 +0200

    fix: Stream examples

--- examples/studio/chat/stream-chat-completions.ts
+++ examples/studio/chat/stream-chat-completions.ts
@@ -1,19 +1,9 @@
-<<<<<<<< HEAD:src/examples/chatExample.ts
-import { AI21 } from '../AI21';
-
-/*
-  This is a temporary example to test the API streaming/non-streaming functionality.
-*/
-========
 import { AI21 } from 'ai21';
 
->>>>>>>> 61d25ea (docs: Added examples):examples/studio/chat/stream-chat-completions.ts
 async function main() {
   const client = new AI21({ apiKey: process.env.AI21_API_KEY });
 
   try {
-    console.log('-------------------------------- streaming flow --------------------------------');
-
     const streamResponse = await client.chat.completions.create({
       model: 'jamba-1.5-mini',
       messages: [{ role: 'user', content: 'Hello, how are you? tell me a 100 line story about a cat' }],
@@ -22,15 +12,6 @@ async function main() {
     for await (const chunk of streamResponse) {
       process.stdout.write(chunk.choices[0]?.delta?.content || '');
     }
-
-    console.log('-------------------------------- non streaming flow --------------------------------');
-
-    const response = await client.chat.completions.create({
-      model: 'jamba-1.5-mini',
-      messages: [{ role: 'user', content: 'Hello, how are you? tell me a 100 line story about a cat' }],
-      stream: false,
-    });
-    console.log(response);
   } catch (error) {
     console.error('Error:', error);
   }
