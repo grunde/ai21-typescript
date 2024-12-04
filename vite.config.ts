49d82c61609010e2fc70edd7f60a56beb85f0d58
asafg <asafg@ai21.com>
2024-12-04 15:26:58 +0200

    ci: Added form-data to bundle

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       formats: ['es', 'cjs', 'umd'],
     },
     rollupOptions: {
-      external: ['node-fetch', 'form-data'],
+      external: ['node-fetch', 'form-data', 'fs', 'stream'],
       output: {
         globals: {
           'node-fetch': 'fetch', 
