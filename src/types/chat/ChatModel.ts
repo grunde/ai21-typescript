8f2178c3233428c147598cfa5906dd58eb4884e1
asafg <asafg@ai21.com>
2025-03-30 10:23:06 +0300

    fix: Formatting

--- src/types/chat/ChatModel.ts
+++ src/types/chat/ChatModel.ts
@@ -1,9 +1,9 @@
-export type ChatModel = 
-'jamba-mini' | 
-'jamba-large' | 
-'jamba-large-1.6-2025-03'| 
-'jamba-mini-1.6-2025-03' | 
-'jamba-large-1.6' | 
-'jamba-mini-1.6' |
-'jamba-1.5-mini' |
-'jamba-1.5-large';
+export type ChatModel =
+  | 'jamba-mini'
+  | 'jamba-large'
+  | 'jamba-large-1.6-2025-03'
+  | 'jamba-mini-1.6-2025-03'
+  | 'jamba-large-1.6'
+  | 'jamba-mini-1.6'
+  | 'jamba-1.5-mini'
+  | 'jamba-1.5-large';
